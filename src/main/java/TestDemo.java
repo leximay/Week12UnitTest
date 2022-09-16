7be09830e7c5637b8bdc8cf597a6f025f03f61fe
leximay <108042396+leximay@users.noreply.github.com>
2022-09-16 18:31:08 -0400

    Add files via upload

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		if(a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("Both parameters must be positive");
+		} else {
+		return a + b;
+	}
+	}
+	int randomNumberSquared() {
+		return getRandomInt() * getRandomInt();
+	}
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+}
